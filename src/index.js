4d64c4f9c54bb96306012a47220501c17479f1c1
CarolinaCapilla <carolina.capilla@gmail.com>
2021-02-22 11:39:00 +0000

    added interceptors and default config for axios headers and baseURL

--- src/index.js
+++ src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+// setting default headers globally
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+// setting default headers for one specific action
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

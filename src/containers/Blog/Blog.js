ff0b45b72b7be0af2e173aa620ee73d2dda427d8
CarolinaCapilla <carolina.capilla@gmail.com>
2021-03-10 13:42:28 +0000

    nested route in Posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+
 import './Blog.css';
 
 class Blog extends Component {
@@ -15,7 +15,7 @@ class Blog extends Component {
             <ul>
               <li>
                 <NavLink
-                  to="/"
+                  to="/posts/"
                   exact
                   activeClassName="my-active"
                   activeStyle={{
@@ -43,9 +43,8 @@ class Blog extends Component {
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
         <Switch>
-          <Route path="/" exact component={Posts} />
           <Route path="/new-post" component={NewPost} />
-          <Route path="/:id" exact component={FullPost} />
+          <Route path="/posts/" component={Posts} />
         </Switch>
       </div>
     );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
+import FullPost from '../FullPost/FullPost';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
 
@@ -31,15 +32,15 @@ class Posts extends Component {
 
   postSelectedHandler = (id) => {
     // this method will also work without then rendering the Link component wrapping Post
-    // this.props.history.push({ pathname: '/' + id });
-    this.setState({ selectedPostId: id });
+    this.props.history.push({ pathname: '/posts/' + id });
+    // this.setState({ selectedPostId: id });
   };
   render() {
     let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
     if (!this.state.error) {
       posts = this.state.posts.map((post) => {
         return (
-          <Link to={'/' + post.id} key={post.id}>
+          <Link to={'/posts/' + post.id} key={post.id}>
             <Post
               // key={post.id}
               title={post.title}
@@ -50,7 +51,16 @@ class Posts extends Component {
         );
       });
     }
-    return <section className="Posts">{posts}</section>;
+    return (
+      <div>
+        <section className="Posts">{posts}</section>;
+        <Route
+          path={this.props.match.url + '/:id'}
+          exact
+          component={FullPost}
+        />
+      </div>
+    );
   }
 }
 
